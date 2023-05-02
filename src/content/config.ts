e5681881ba93d33547bbd0c0dbfe44505dbfa77b
Melih Darcan <57872471+MelihDarcanxyz@users.noreply.github.com>
2023-05-02 13:44:31 +0300

    feat: define pages, blog, projects collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,8 @@
-// 1. Import utilities from `astro:content`
+
 import { z, defineCollection } from 'astro:content';
-// 2. Define a schema for each collection you'd like to validate.
+
+const validTags = z.enum([""])
+
 const defaultCollection = defineCollection({
   schema: z.object({
     title: z.string(),
@@ -12,6 +14,41 @@ const defaultCollection = defineCollection({
     image: z.string().optional(),
   }),
 });
+
+const pagesCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    draft: z.boolean()
+  })
+})
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.date(),
+    tags: z.array(validTags).transform((arr) => new Set(arr)),
+    image: z.string().url(),
+    draft: z.boolean()
+  })
+})
+
+// Projects
+const projectsCollection = defineCollection({
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    url: z.string().url(),
+    actively_developed: z.boolean(),
+    image: z.string().optional(),
+    draft: z.boolean(),
+  }),
+});
+
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
+  'pages': pagesCollection,
+  'blog': blogCollection,
+  'projects': projectsCollection
 };
