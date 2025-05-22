bd9d1bc6c817d02cc7621e4d0c052b43a93a8143
Melih DARCAN <57872471+MelihDarcanxyz@users.noreply.github.com>
2025-05-23 01:42:12 +0300

    feat: add mathjax support

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,42 +10,50 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkMdx from "remark-mdx";
 import { remarkModifiedTime } from "./src/plugins/remark-modified-time.mjs";
 import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
+import remarkMath from "remark-math";
+import rehypeMathjax from "rehype-mathjax";
 
 import og from "astro-og";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://melihdarcan.dev",
-    output: "static",
-    compressHTML: true,
+  site: "https://melihdarcan.dev",
+  output: "static",
+  compressHTML: true,
 
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 
-    integrations: [icon(), mdx(), og()],
+  integrations: [icon(), mdx(), og()],
 
-    markdown: {
-        remarkPlugins: [remarkMdx, remarkReadingTime, remarkModifiedTime],
+  markdown: {
+    remarkPlugins: [
+      remarkMdx,
+      remarkReadingTime,
+      remarkModifiedTime,
+      remarkMath,
+    ],
 
-        rehypePlugins: [
-            rehypeHeadingIds,
-            [
-                rehypeAutolinkHeadings,
-                {
-                    behavior: "wrap",
-                },
-            ],
-        ],
-    },
+    rehypePlugins: [
+      rehypeHeadingIds,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+        },
+      ],
+      rehypeMathjax,
+    ],
+  },
 
-    experimental: {
-        fonts: [
-            {
-                provider: fontProviders.fontsource(),
-                name: "JetBrains Mono",
-                cssVariable: "--main-font",
-            },
-        ],
-    },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.fontsource(),
+        name: "JetBrains Mono",
+        cssVariable: "--main-font",
+      },
+    ],
+  },
 });
